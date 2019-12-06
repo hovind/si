65d6738b730c2ec45e608e8c634b774da6661995
Øystein Hovind <oystein.hovind@folq.no>
2019-12-06 17:55:46 +0100

    Add more units and macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,74 @@
 #![feature(const_generics)]
+#![feature(type_ascription)]
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let x : Kilogram<i64> = Si(1);
-        let y : Kilogram<i64> = Si(2);
-        assert_eq!(x + x, y);
+        let kg = kg!(1i64);
+        let m = metre!(1);
+        let s = second!(1);
+        let newton = kg * m / s / s;
+        println!("Hopefully newtons: {}", newton)
     }
 }
 
 use std::ops::{Add, Div, Mul, Sub};
+use std::fmt::{Display};
+use std::fmt;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Si<T, const S: isize, const MET: isize, const KG: isize, const A: isize, const K: isize, const MOL: isize, const CD: isize>(T);
 
-type Kilogram<T> = Si<T, 0, 0, 1, 0, 0, 0, 0>;
+pub type Second<T>   = Si<T, 1, 0, 0, 0, 0, 0, 0>;
+pub type Metre<T>    = Si<T, 0, 1, 0, 0, 0, 0, 0>;
+pub type Kilogram<T> = Si<T, 0, 0, 1, 0, 0, 0, 0>;
+pub type Ampere<T>   = Si<T, 0, 0, 0, 1, 0, 0, 0>;
+pub type Kelvin<T>   = Si<T, 0, 0, 0, 0, 1, 0, 0>;
+pub type Mole<T>     = Si<T, 0, 0, 0, 0, 0, 1, 0>;
+pub type Candela<T>  = Si<T, 0, 0, 0, 0, 0, 0, 1>;
+pub type Newton<T>   = Si<T, -2, 1, 1, 0, 0, 0, 0>;
+
+macro_rules! implement {
+    ( $type:ident, $name:ident ) => {
+        #[macro_export]
+        macro_rules! $name {
+            ($x:expr) => { Si($x) : $type<_> }
+        }
+    }
+}
+
+implement!(Second, second);
+implement!(Metre, metre);
+implement!(Kilogram, kg);
+implement!(Ampere, ampere);
+implement!(Kelvin, kelvin);
+implement!(Mole, mole);
+implement!(Candela, cd);
 
 impl<T, const S: isize, const MET: isize, const KG: isize, const A: isize, const K: isize, const MOL: isize, const CD: isize>
     Si<T, { S }, { MET }, { KG }, { A }, { K }, { MOL }, { CD }>
 {
     pub fn kg(a: T) -> Kilogram<T> {
         Si(a)
+    }
+
+}
+
+impl<T> Display for Kilogram<T> where
+    T: Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} kg", self.0)
+    }
+}
+
+impl<T> Display for Newton<T> where
+    T: Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} N", self.0)
     }
 }
 
