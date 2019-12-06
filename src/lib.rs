f289b709725b5e8707a769f572ce0dae44a7b9e8
Øystein Hovind <oystein.hovind@folq.no>
2019-12-06 16:54:50 +0100

    Add `Sub` and `Div` traits

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,13 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(Si::kg(1) + Si::kg(1), Si::kg(2));
+        let x : Kilogram<i64> = Si(1);
+        let y : Kilogram<i64> = Si(2);
+        assert_eq!(x + x, y);
     }
 }
 
-use std::ops::{Add, Mul};
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Si<T, const S: isize, const MET: isize, const KG: isize, const A: isize, const K: isize, const MOL: isize, const CD: isize>(T);
@@ -36,6 +38,18 @@ where
     }
 }
 
+impl<T, const S: isize, const MET: isize, const KG: isize, const A: isize, const K: isize, const MOL: isize, const CD: isize>
+    Sub for Si<T, { S }, { MET }, { KG }, { A }, { K }, { MOL }, { CD }>
+where
+    T: Sub<T, Output = T>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self(self.0.sub(rhs.0))
+    }
+}
+
 impl<T
     , const S1: isize, const MET1: isize, const KG1: isize, const A1: isize, const K1: isize, const MOL1: isize, const CD1: isize
     , const S2: isize, const MET2: isize, const KG2: isize, const A2: isize, const K2: isize, const MOL2: isize, const CD2: isize>
@@ -48,5 +62,20 @@ where
 
     fn mul(self, rhs: Si<T, { S2 }, { MET2 }, { KG2 }, { A2 }, { K2 }, { MOL2 }, { CD2 }>) -> Self::Output {
         Self(self.0.mul(rhs.0))
+    }
+}
+
+impl<T
+    , const S1: isize, const MET1: isize, const KG1: isize, const A1: isize, const K1: isize, const MOL1: isize, const CD1: isize
+    , const S2: isize, const MET2: isize, const KG2: isize, const A2: isize, const K2: isize, const MOL2: isize, const CD2: isize>
+    Div<Si<T, { S2 }, { MET2 }, { KG2 }, { A2 }, { K2 }, { MOL2 }, { CD2 }>>
+    for Si<T, { S1 }, { MET1 }, { KG1 }, { A1 }, { K1 }, { MOL1 }, { CD1 }>
+where
+    T: Div<T, Output = T>,
+{
+    type Output = Si<T, { S1 - S2 }, { MET1 - MET2 }, { KG1 - KG2 }, { A1 - A2 }, { K1 - K2 }, { MOL1 - MOL2 }, { CD1 - CD2 }>;
+
+    fn div(self, rhs: Si<T, { S2 }, { MET2 }, { KG2 }, { A2 }, { K2 }, { MOL2 }, { CD2 }>) -> Self::Output {
+        Self(self.0.div(rhs.0))
     }
 }
